f6377773594c7866a4f88b8bb1e37523456cada5
Andrew Ross <andrew.ross@kurtosys.com>
2016-02-06 15:48:42 +0200

    Cleanup up the project

--- typings/browser.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="browser/ambient/expect/expect.d.ts" />
-/// <reference path="browser/ambient/react/react.d.ts" />
--- typings/main.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="main/ambient/expect/expect.d.ts" />
-/// <reference path="main/ambient/react/react.d.ts" />
